f7643309f7b00fb03ed83739d6dba73e185ffccf
Meet Sardhara <meet.scalelot@gmail.com>
2023-11-29 19:27:21 +0530

    User product Get APIs done 29-11-2023

--- routes/users/product.js
+++ routes/users/product.js
@@ -8,14 +8,31 @@ const constants = require('../../utilities/constants');
 const helper = require('../../utilities/helper');
 const userModel = require('../../models/users/users.model');
 const productModel = require('../../models/admin/products.model');
+const sizeMasterModel = require('../../models/admin/size.master');
 
-router.get('/' , helper.authenticateToken , async (req , res) => {
+router.post('/' , helper.authenticateToken , async (req , res) => {
+  const {page , limit , search} = req.body;
   if(req.token._id && mongoose.Types.ObjectId.isValid(req.token._id)){
     let primary = mongoConnection.useDb(constants.DEFAULT_DB);
     let userData = await primary.model(constants.MODELS.users , userModel).findById(req.token._id).lean();
-    if(userData && userData != null){
-      let allProducts = await primary.model(constants.MODELS.products , productModel).find({status: true}).sort({createdAt: -1}).select('-createdBy -updatedBy -createdAt -updatedAt -__v').lean();
-      return responseManager.onSuccess('All Available products details...!' , allProducts , res);
+    if(userData && userData != null && userData.status === true){
+      await primary.model(constants.MODELS.products, productModel).paginate({
+        $or: [
+          {title: {$regex: search, $options: 'i'}}
+        ],
+        status: true
+      }, {
+        page,
+        limit: parseInt(limit),
+        select: '-createdBy -updatedBy -createdAt -updatedAt -__v',
+        populate: {path: 'productDetails.size' , model: primary.model(constants.MODELS.sizemasters, sizeMasterModel) , select: '_id size_name'},
+        sort: {createdAt: -1},
+        lean: true
+      }).then((products) => {
+        return responseManager.onSuccess('Products details...!' , products , res);
+      }).catch((error) => {
+        return responseManager.onError(error , res);
+      });
     }else{
       return responseManager.badrequest({message: 'Invalid token to get user, please try again'}, res);
     }
@@ -24,15 +41,19 @@ router.get('/' , helper.authenticateToken , async (req , res) => {
   }
 });
 
-router.post('/product' , helper.authenticateToken , async (req , res) => {
+router.post('/getone' , helper.authenticateToken , async (req , res) => {
   const {productId} = req.body;
   if(req.token._id && mongoose.Types.ObjectId.isValid(req.token._id)){
     let primary = mongoConnection.useDb(constants.DEFAULT_DB);
     let userData = await primary.model(constants.MODELS.users , userModel).findById(req.token._id).lean();
     if(userData && userData != null){
       if(productId && productId.trim() != '' && mongoose.Types.ObjectId.isValid(productId)){
-        let productData = await primary.model(constants.MODELS.products , productModel).findById(productId).select('-createdBy -updatedBy -createdAt -updatedAt -__v').lean();
-        if(productData && productData != null){
+        let productData = await primary.model(constants.MODELS.products , productModel).findById(productId).select('-createdBy -updatedBy -createdAt -updatedAt -__v').populate({
+          path: 'productDetails.size',
+          model: primary.model(constants.MODELS.sizemasters, sizeMasterModel),
+          select: '_id size_name'
+        }).lean();
+        if(productData && productData != null && productData.status === true){
           return responseManager.onSuccess('Product details...!' , productData , res);
         }else{
           return responseManager.badrequest({message: 'Invalid producId to get product details, please try again...!'}, res);
