0963a592dbbbde8234ab31d584f05b16e298e03b
Meet Sardhara <140481902+meetscalelot@users.noreply.github.com>
2023-12-22 16:07:37 +0530

    download invoice done for admin 22-12-2023

--- utilities/aws.js
+++ utilities/aws.js
@@ -22,6 +22,10 @@ const fileName = (fName , extn) => {
   return `${fName}-${Date.now().toString()}.${extn}`;
 }
 
+const invoiceFileName = (fName , invoice_no , extn) => {
+  return `${fName}-${invoice_no}.${extn}`;
+}
+
 const bucket = process.env.AWS_BUCKET_NAME;
 
 async function saveToS3(buffer, parentfolder, contentType, sendorreceive) {
@@ -88,4 +92,36 @@ async function saveToS3WithName(buffer , parentfolder , contentType , sendorrece
   return promise;
 };
 
-module.exports = {saveToS3 , saveToS3WithName};
+async function saveToS3WithInvoiceNo(buffer , parentfolder , contentType , invoice_no) {
+  let promise = new Promise(function (resolve , reject) {
+    let newContentType = contentType.split(";");
+    let blobName = "";
+    allowedContentTypes.allowedContentTypes.some((element, index) => {
+      if(element.mimeType == newContentType[0]){
+        blobName = parentfolder + '/' + invoiceFileName(element.fName, invoice_no , element.extn);
+      }
+    });
+    if (blobName != "") {
+      var putParams = {
+        Bucket: bucket,
+        Key: blobName,
+        Body: buffer,
+        ContentType: contentType
+      };
+      const command = new PutObjectCommand(putParams);
+      s3Client.send(command).then((data) => {
+        if (data && data.$metadata && data.$metadata.httpStatusCode && data.$metadata.httpStatusCode == 200) {
+          let data = { Key: blobName };
+          resolve({ msg: 'file uploaded successfully', data });
+        } else {
+          reject(new Error({ msg: 'An error occurred while completing the upload' }));
+        }
+      }).catch((error) => {
+        reject(new Error({ msg: 'An error occurred while completing the upload' }));
+      });
+    } else { reject(new Error({ msg: 'Invalid file name to upload file on cloud' })); }
+  });
+  return promise;
+};
+
+module.exports = {saveToS3 , saveToS3WithName, saveToS3WithInvoiceNo};
